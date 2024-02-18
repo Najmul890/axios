98c7aac30523945c00c3d234c110e52539005653
Najmul890 <najmul890890@gmail.com>
2024-02-18 15:15:06 +0600

    updated

--- src/api/api.js
+++ src/api/api.js
@@ -4,32 +4,32 @@ const api = axios.create({
     baseURL: "http://localhost:8000",
 });
 
-// const token = "jhdjfhdjfhdjfh";
+const token = "jhdjfhdjfhdjfh";
 
-// // request interceptor
-// api.interceptors.request.use(
-//     (config) => {
-//         config.headers["Authorization"] = "Bearer " + token;
-//         return config;
-//     },
-//     (error) => {
-//         return Promise.reject(error);
-//     }
-// );
+// request interceptor
+api.interceptors.request.use(
+    (config) => {
+        config.headers["Authorization"] = "Bearer " + token;
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
-// // response interceptor
-// api.interceptors.response.use(
-//     (response) => {
-//         return response;
-//     },
-//     (err) => {
-//         if (err.response) {
-//             // error came from server
-//             err.message = `Error from server: status: ${err.response.status} - message: ${err.response.statusText}`;
-//         }
+// response interceptor
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (err) => {
+        if (err.response) {
+            // error came from server
+            err.message = `Error from server: status: ${err.response.status} - message: ${err.response.statusText}`;
+        }
 
-//         return Promise.reject(err);
-//     }
-// );
+        return Promise.reject(err);
+    }
+);
 
 export default api;
